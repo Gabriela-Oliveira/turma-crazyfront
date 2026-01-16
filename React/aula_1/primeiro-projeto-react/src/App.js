a5b5c43cb5c3d8f0a7339d1e069595d7d0a40c34
Gabriela-Oliveira <eradigitalaquarius@gmail.com>
2026-01-15 21:59:44 -0300

    Add aula de revisão do js avançado e da primeira aula de React

---
+++ React/aula_1/primeiro-projeto-react/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Bem vindos ao módulo de React</h1>
+        <p>Neste módulo vcs vão zerar o GAME</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
